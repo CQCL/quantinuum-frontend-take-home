4b9f449d6fa4aa798f1415499136bfc6abe6c1e7
aidanCQ <aidan.keay@quantinuum.com>
2024-06-07 13:42:35 +0100

    Use app file.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button, Input, Switch } from "@cqcl/quantinuum-ui";
+
+export default function App(){
+    return <div>
+        <Button variant="outline">Hello</Button>
+        <Input placeholder="say something"></Input>
+        <Switch ></Switch>
+    </div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Button, Input, Switch} from '@cqcl/quantinuum-ui'
+
 import '@cqcl/quantinuum-ui/tokens.css'
 import './main.css'
+import App from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Button variant="outline">Hello</Button>
-    <Input placeholder="say something"></Input>
-    <Switch ></Switch>
-    {/* <div className='bg-red-600 w-20 h-20'> Hello</div> */}
+    <App/>
   </React.StrictMode>,
 )
